7bda9c1fdafd3a3d292bac5ff1a05f89b9e5bfce
vater <v4ter@hotmail.com>
2012-07-12 18:26:50 +0300

    functionality done, todo templating

--- test/faq_test.js
+++ test/faq_test.js
@@ -3,19 +3,29 @@ $(function(){
 
 	module('A', {
 		setup: function(){
-			$('.faq-block:first a').trigger('click');
-			$('.faq-block:eq(3) a').trigger('click');
-			this.link = $('.faq-block:eq(2) a').trigger('click');
+			$('.faq-q:first').trigger('click');
+			$('.faq-q:eq(3)').trigger('click');
+			this.link = $('.faq-q:eq(2)').trigger('click');
 			this.block = this.link.closest('.faq-block');
+			this.otherLinks = $('.faq-q').not(this.link);
+			this.otherBlocks = $('.faq-block').not(this.block);
 		}
 	});
 
 	test('clicked has active class', 1, function(){
-		ok(this.link.hasClass('active'));
+		ok(this.block.hasClass('active'));
 	});
 
 	test('no other element has active class', 1, function(){
-		equal($('.faq-q a').not(this.link).filter('.active').length, 0);
+		equal(this.otherBlocks.filter('.active').length, 0);
+	});
+
+	test('clicked has no vertical bg', 1, function(){
+		equal(this.link.css('background'), '');
+	});
+
+	test('all other elements have vertical bg', 1, function(){
+		equal(this.otherLinks.filter('.no-vertical').length, 0);
 	});
 
 });
